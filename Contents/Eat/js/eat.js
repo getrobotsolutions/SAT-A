d258bb0fa7b7b15a706a362145413698988dd7bd
satishharkal <satish.harkal26@gmail.com>
2018-04-06 13:10:34 -0700

    404 image path change

--- Contents/Eat/js/eat.js
+++ Contents/Eat/js/eat.js
@@ -41,7 +41,7 @@ location.reload();
 
 var Hotel_en = new Array ( );
 Hotel_en[0] = new Array ( "Auntie Anne's/Cinnabon","The fresh, gooey taste of a Cinnabon cinnamon roll accompanied with a deliciously perfected Auntie Anne’s pretzel offers SAT passengers a sweet treat on the go.", "images/2.png" , "images/offers/Auntie_Anne/1.png");
-Hotel_en[1] = new Array ( "Blimpies","", "images/Blimpies.jpeg" , "images/offers/Auntie_Anne/404.png");
+Hotel_en[1] = new Array ( "Blimpies","", "images/Blimpies.jpeg" , "images/offers/404.png");
 Hotel_en[2] = new Array ( "Dunkin' Donuts","More than 2.7 million Americans a day can’t be wrong! Offering more than just donuts, the No. 1 U.S. retailer of coffee has been keeping America running since 1950. Let Dunkin’ Donuts give you that kick to your destination.", "images/5.png", "images/offers/404.png" );
 Hotel_en[3] = new Array ( "Famous Famiglia Pizzeria","It’s the pizza New Yorkers vote No. 1 and the offcial pie of the New York Yankees and Madison Square Garden. Famous Famiglia offers traditional Italian at its best, including pasta, calzones, heroes and salads. Buon appetito!", "images/6.png" , "images/offers/404.png");
 Hotel_en[4] = new Array ( "Fruteria","From fruit-infused cocktails to Mexican tapas, Fruteria offers a variety of items while providing the experience of Mexico’s beautiful and colorful fruit stands.", "images/7.png", "images/offers/La Fruteria/La Fruteria-1.png" );
@@ -53,7 +53,7 @@ Hotel_en[9] = new Array ( "Mission City Ice House","Have time to spare? Then coo
 Hotel_en[10] = new Array ( "Natalie's Candy Jar","Giant lollipops mark the spot that offers vintage candies, deluxe chocolates and every gummy variety under the rainbow, and that just scratches the sugar-coated surface.", "images/13.png" , "images/offers/404.png");
 Hotel_en[11] = new Array ( "Raising Cane's Chicken Fingers","We only have one love: chicken fingers. Add Texas Toast, our specialty sauce and crinkle cut fries and you have a made-to-order meal to cluck about..", "images/14.png", "images/offers/Raising Cane's/Raising Cane's-1.png" );
 Hotel_en[12] = new Array ( "R Sala Bebida Botana Bar","R Sala Bebida Botana Bar goers passengers a relaxed lounge, serving light fare with a distinctive San Antonio Mex-Chic flavor, while Starbucks goers the world’s most recognizable brand of coffee — all to create a destination to relax and recharge.", "images/15.png", "images/offers/R-Salsa/R-Salsa-1.png" );
-Hotel_en[13] = new Array ( "SA Express News","", "images/Stars of San Antonio.jpeg" , "images/offers/Auntie_Anne/404.png");
+Hotel_en[13] = new Array ( "SA Express News","", "images/Stars of San Antonio.jpeg" , "images/offers/404.png");
 Hotel_en[14] = new Array ( "Steak 'n Shake","Proudly serving America’s best, freshest, and tastiest burgers and shakes for more than 75 years. Add a side of fries and you’ll surely satisfy your hunger.", "images/19.png", "images/offers/Steak n' Shake/Steak n' Shake-1.png" );
 Hotel_en[15] = new Array ( "Vino Volo","Boutique retail meets lounging in style at Vino Volo, where great wines from across the globe are sold to travelers by glass or by bottle, en route or pre-flight. Travel the world, glass by glass!", "images/20.png", "images/offers/Vino Volo/Vino Volo-1.png" );
 
@@ -61,7 +61,7 @@ Hotel_en[15] = new Array ( "Vino Volo","Boutique retail meets lounging in style 
 var Hotel_sp = new Array ( );
 
 Hotel_sp[0] = new Array ( "Auntie Anne's/Cinnabon","El sabor fresco y pegajoso de un rollo de canela Cinnabon acompañado con un pretzel de la Auntie Anne deliciosamente perfeccionado ofrece a los pasajeros del SAT un dulce regalo en el camino.", "images/2.png", "images/offers/Auntie_Anne/1.png" );
-Hotel_sp[1] = new Array ( "Blimpies","", "images/Blimpies.jpeg" , "images/offers/Auntie_Anne/404.png");
+Hotel_sp[1] = new Array ( "Blimpies","", "images/Blimpies.jpeg" , "images/offers/404.png");
 Hotel_sp[2] = new Array ( "Dunkin' Donuts","Más de 2.7 millones de Americanos al día no pueden estar equivocados! Ofreciendo algo más que rosquillas, el mayor minorista de café de EE. UU. Ha mantenido a América en funcionamiento desde 1950. Permita que Dunkin 'Donuts le dé esa patada a su destino.", "images/5.png" , "images/offers/404.png");
 Hotel_sp[3] = new Array ( "Famous Famiglia Pizzeria","Es la pizza que los Neoyorquinos votan n. ° 1 y el pastel oficial de los Yankees de Nueva York y el Madison Square Garden. El famoso Famiglia ofrece lo mejor del italiano tradicional, como pasta, calzones, héroes y ensaladas. Buon appetito!", "images/6.png" , "images/offers/404.png");
 Hotel_sp[4] = new Array ( "Fruteria","Desde cócteles infundidos con frutas hasta tapas mexicanas, Fruteria ofrece una variedad de artículos mientras brinda la experiencia de los hermosos y coloridos puestos de frutas de México.", "images/7.png", "images/offers/La Fruteria/La Fruteria-1.png" );
@@ -73,7 +73,7 @@ Hotel_sp[9] = new Array ( "Mission City Ice House","Tienes tiempo de sobra? Lueg
 Hotel_sp[10] = new Array ( "Natalie's Candy Jar","Las piruletas gigantes marcan el lugar que ofrece dulces vintage, chocolates de lujo y cada variedad gomosa bajo el arco iris, y que solo araña la superficie recubierta de azúcar.", "images/13.png" , "images/offers/404.png");
 Hotel_sp[11] = new Array ( "Raising Cane's Chicken Fingers","Solo tenemos un amor: dedos de pollo. Agregue Texas Toast, nuestra especialidad de salsa y papas fritas rizadas  y usted tendrá una comida hecha por encargo para cacarear.", "images/14.png", "images/offers/Raising Cane's/Raising Cane's-1.png" );
 Hotel_sp[12] = new Array ( "R Sala Bebida Botana Bar","R Sala Bebida Botana Bar ofrece a los pasajeros un lounge relajado, que sirve comida ligera con un distintivo sabor Mex-Chic de San Antonio, mientras que Starbucks disfruta de la marca de café más reconocida del mundo, todo para crear un destino para relajarse y recargar energías.", "images/15.png", "images/offers/R-Salsa/R-Salsa-1.png" );
-Hotel_sp[13] = new Array ( "SA Express News","", "images/Stars of San Antonio.jpeg" , "images/offers/Auntie_Anne/404.png");
+Hotel_sp[13] = new Array ( "SA Express News","", "images/Stars of San Antonio.jpeg" , "images/offers/404.png");
 Hotel_sp[14] = new Array ( "Steak 'n Shake","Orgullosamente sirviendo las mejores y más frescas hamburguesas y batidos de los Estados Unidos durante más de 75 años. Agregue un lado de las papas fritas y seguramente saciará su hambre.", "images/19.png", "images/offers/Steak n' Shake/Steak n' Shake-1.png" );
 Hotel_sp[15] = new Array ( "Vino Volo","Boutique minor se encuentra con estilo en Vino Volo, donde los grandes vinos de todo el mundo se venden a los viajeros por copa o en botella, en ruta o antes del vuelo. Viaja por el mundo, vidrio por vidrio! ", "images/20.png", "images/offers/Vino Volo/Vino Volo-1.png" );
 
